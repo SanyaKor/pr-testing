c96209f874824be0e077a46c1650796323712f88
Aleksandr Lebedev <alexandr.2nd@gmail.com>
2025-10-23 23:41:52 +0200

    Update py_script.py

--- testing/py_script.py
+++ testing/py_script.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python3
+
 import os, json, urllib.request, urllib.error
 
 repo  = os.environ["REPO"]
 pr    = os.environ["PR_NUMBER"]
 token = os.environ["GITHUB_TOKEN"]
-
-parts = ref.split("/")
-if len(parts) < 3 or not parts[2].isdigit():
-    raise SystemExit("ERROR: unabble to fetch PR from GITHUB_REF: " + ref)
-pr_number = parts[2]
-
 
 url  = f"https://api.github.com/repos/{repo}/issues/{pr}/comments"
 data = json.dumps({"body": "✅ This Pull Request has been taken in progress by the external bot."}).encode()
